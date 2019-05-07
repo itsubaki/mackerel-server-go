ee6a88c1d7ad89718ccdcba47b9b585913f5eeca
itsubaki <itsubaki@users.noreply.github.com>
2019-05-07 14:45:15 +0900

    Add graph annotation

--- pkg/domain/graph.go
+++ pkg/domain/graph.go
@@ -6,3 +6,16 @@ type GraphDef struct {
 	Unit        string   `json:"unit,omitempty"`
 	Metrics     []Metric `json:"metrics"`
 }
+
+type GraphAnnotations struct {
+	GraphAnnotations []GraphAnnotation `json:"graphAnnotations"`
+}
+
+type GraphAnnotation struct {
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	From        int64    `json:"from"`
+	To          int64    `json:"to"`
+	Service     string   `json:"service"`
+	Roles       []string `json:"roles"`
+}
