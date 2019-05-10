005736c3bdc42351f56fa8d179c7918d4bf48770
itsubaki <itsubaki@users.noreply.github.com>
2019-05-10 19:28:56 +0900

    Update

--- pkg/interfaces/database/user_repository.go
+++ pkg/interfaces/database/user_repository.go
@@ -1,6 +1,10 @@
 package database
 
-import "github.com/itsubaki/mackerel-api/pkg/domain"
+import (
+	"strings"
+
+	"github.com/itsubaki/mackerel-api/pkg/domain"
+)
 
 type UserRepository struct {
 	SQLHandler
@@ -8,6 +12,10 @@ type UserRepository struct {
 
 func NewUserRepository(handler SQLHandler) *UserRepository {
 	handler.Transact(func(tx Tx) error {
+		if _, err := tx.Exec("create table if not exists users (id int, screen_name varchar(128), email varchar(128), authority varchar(128), is_in_registeration_process boolean, is_mfa_enabled boolean, authentication_methods varchar(128), joined_at bigint)"); err != nil {
+			return err
+		}
+
 		return nil
 	})
 
@@ -17,7 +25,38 @@ func NewUserRepository(handler SQLHandler) *UserRepository {
 }
 
 func (repo *UserRepository) List() (*domain.Users, error) {
-	return nil, nil
+	var users []domain.User
+
+	repo.Transact(func(tx Tx) error {
+		rows, err := tx.Query("select * from users")
+		if err != nil {
+			return err
+		}
+
+		for rows.Next() {
+			var user domain.User
+			var method string
+			if err := rows.Scan(
+				&user.ID,
+				&user.ScreenName,
+				&user.Email,
+				&user.Authority,
+				&user.IsInRegisterationProcess,
+				&user.IsMFAEnabled,
+				method,
+				&user.JoinedAt,
+			); err != nil {
+				return err
+			}
+			user.AuthenticationMethods = strings.Split(method, ",")
+
+			users = append(users, user)
+		}
+
+		return nil
+	})
+
+	return &domain.Users{Users: users}, nil
 }
 
 func (repo *UserRepository) Delete(userID string) (*domain.User, error) {
@@ -33,18 +72,20 @@ func (repo *UserRepository) Delete(userID string) (*domain.User, error) {
 			return err
 		}
 
+		var method string
 		if err := rows.Scan(
 			&user.ID,
 			&user.ScreenName,
 			&user.Email,
 			&user.Authority,
-			&user.AuthenticationMethods,
 			&user.IsInRegisterationProcess,
 			&user.IsMFAEnabled,
+			method,
 			&user.JoinedAt,
 		); err != nil {
 			return err
 		}
+		user.AuthenticationMethods = strings.Split(method, ",")
 
 		return nil
 	})
