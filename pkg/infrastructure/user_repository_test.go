b5e23bcc733f92ed8b8950756d10af8731e10bef
itsubaki <itsubaki@users.noreply.github.com>
2019-05-31 12:15:37 +0900

    Update

--- pkg/infrastructure/user_repository_test.go
+++ pkg/infrastructure/user_repository_test.go
@@ -48,6 +48,62 @@ func TestUserRepositorySave(t *testing.T) {
 	}
 }
 
+func TestUserRepositoryExists(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := database.UserRepository{}
+	repo.SQLHandler = &SQLHandler{db}
+	defer repo.Close()
+
+	orgID := domain.NewOrgID()
+	user := domain.User{
+		ID:                      "example001",
+		ScreenName:              "example001.screen",
+		Email:                   "example@example.com",
+		Authority:               "owner",
+		IsInRegistrationProcess: false,
+		IsMFAEnabled:            false,
+		AuthenticationMethods:   []string{"google", "github"},
+		JoinedAt:                time.Now().Unix(),
+	}
+
+	mock.ExpectBegin()
+	mock.ExpectExec(`insert into users`).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	if err := repo.Save(orgID, &user); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
+
+	rows := sqlmock.NewRows(
+		[]string{
+			"1",
+		}).
+		AddRow(
+			1,
+		)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`select 1 from users where org_id=? and id=? limit 1`)).
+		WithArgs(orgID, user.ID).
+		WillReturnRows(rows)
+
+	if !repo.Exists(orgID, user.ID) {
+		t.Fatal("exists failed")
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestUserRepositoryList(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
