9ad29eedf32b6bbe8228385aeeee95041cd85c28
itsubaki <itsubaki@users.noreply.github.com>
2019-06-12 13:39:14 +0900

    Add create table to notification groups/channels

--- pkg/interfaces/database/channel_repository.go
+++ pkg/interfaces/database/channel_repository.go
@@ -1,12 +1,77 @@
 package database
 
-import "github.com/itsubaki/mackerel-api/pkg/domain"
+import (
+	"fmt"
+
+	"github.com/itsubaki/mackerel-api/pkg/domain"
+)
 
 type ChannelRepository struct {
 	SQLHandler
 }
 
 func NewChannelRepository(handler SQLHandler) *ChannelRepository {
+	if err := handler.Transact(func(tx Tx) error {
+		if _, err := tx.Exec(
+			`
+			create table if not exists channels (
+				org_id              varchar(64) not null,
+				id                  varchar(16) not null primary key,
+				name                varchar(16) not null,
+				type                varchar(16) not null,
+				url                 text,
+				mentions            text,
+				enabled_graph_image bool not null default '1'
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create tables channels: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists channel_events (
+				org_id     varchar(64) not null,
+				channel_id varchar(16) not null,
+				event      varchar(16) not null,
+				primary key(channel_id, event)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create tables channel_events: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists channel_emails (
+				org_id     varchar(64) not null,
+				channel_id varchar(16) not null,
+				email      varchar(16) not null,
+				primary key(channel_id, email)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create tables channel_email: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists channel_user_ids (
+				org_id     varchar(64) not null,
+				channel_id varchar(16) not null,
+				user_id    varchar(16) not null,
+				primary key(channel_id, user_id)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create tables channel_user_ids: %v", err)
+		}
+
+		return nil
+	}); err != nil {
+		panic(fmt.Errorf("transaction: %v", err))
+	}
+
 	return &ChannelRepository{
 		SQLHandler: handler,
 	}
--- pkg/interfaces/database/group_repository.go
+++ pkg/interfaces/database/group_repository.go
@@ -1,12 +1,87 @@
 package database
 
-import "github.com/itsubaki/mackerel-api/pkg/domain"
+import (
+	"fmt"
+
+	"github.com/itsubaki/mackerel-api/pkg/domain"
+)
 
 type NotificationGroupRepository struct {
 	SQLHandler
 }
 
 func NewNotificationGroupRepository(handler SQLHandler) *NotificationGroupRepository {
+	if err := handler.Transact(func(tx Tx) error {
+		if _, err := tx.Exec(
+			`
+			create table if not exists notification_groups (
+				org_id varchar(64) not null,
+				id     varchar(16) not null primary key,
+				name   varchar(16) not null,
+				level  enum('all', 'critical') not null default 'all'
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create table notification_groups: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists notification_group_children (
+				org_id   varchar(64) not null,
+				group_id varchar(16) not null,
+				child_id varchar(16) not null,
+				primary key(group_id, child_id)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create table notification_group_children: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists notification_group_channels (
+				org_id     varchar(64) not null,
+				group_id   varchar(16) not null,
+				channel_id varchar(16) not null,
+				primary key(group_id, channel_id)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create table notification_group_channels: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists notification_group_monitors (
+				org_id     varchar(64) not null,
+				group_id   varchar(16) not null,
+				monitor_id varchar(16) not null,
+				primary key(group_id, monitor_id)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create table notification_group_monitors: %v", err)
+		}
+
+		if _, err := tx.Exec(
+			`
+			create table if not exists notification_group_services (
+				org_id       varchar(64) not null,
+				group_id     varchar(16) not null,
+				service_name varchar(16) not null,
+				primary key(group_id, service_name)
+			)
+			`,
+		); err != nil {
+			return fmt.Errorf("create table notification_group_servivces: %v", err)
+		}
+
+		return nil
+	}); err != nil {
+		panic(fmt.Errorf("transaction: %v", err))
+	}
+
 	return &NotificationGroupRepository{
 		SQLHandler: handler,
 	}
