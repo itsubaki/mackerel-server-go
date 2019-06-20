d7381270e412deb67e295065c7584b4d64c54651
itsubaki <itsubaki@users.noreply.github.com>
2019-06-20 16:13:04 +0900

    Update router test

--- pkg/infrastructure/router_test.go
+++ pkg/infrastructure/router_test.go
@@ -3,18 +3,42 @@ package infrastructure
 import (
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
-func TestRouter(t *testing.T) {
-	req := httptest.NewRequest("GET", "/", nil)
-	req.Header.Add("X-Api-key", "2684d06cfedbee8499f326037bb6fb7e8c22e73b16bb")
-	rec := httptest.NewRecorder()
+var xapikey = "2684d06cfedbee8499f326037bb6fb7e8c22e73b16bb"
 
+func TestRouter(t *testing.T) {
 	handler := NewSQLHandler()
 	router := Router(handler)
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Add("X-Api-key", xapikey)
+	rec := httptest.NewRecorder()
 	router.ServeHTTP(rec, req)
 
 	if rec.Code != 200 {
 		t.Fatalf("code: %v", rec.Code)
+	}
+}
+
+func TestRouterHosts(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+
+	handler := NewSQLHandler()
+	router := Router(handler)
+
+	req := httptest.NewRequest("GET", "/api/v0/hosts", nil)
+	req.Header.Add("X-Api-key", xapikey)
+	rec := httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+
+	if rec.Code != 200 {
+		t.Fatalf("code: %v", rec.Code)
+	}
+
+	if rec.Body.String() != `{"hosts":[]}` {
+		t.Fatalf("body: %v", rec.Body.String())
 	}
 }
