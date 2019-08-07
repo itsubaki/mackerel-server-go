38a9280c235d245dc5c71d1d96cc8b3fd3b62a9b
itsubaki <itsubaki@users.noreply.github.com>
2019-08-07 18:14:26 +0900

    Add config_test

---
+++ pkg/infrastructure/config_test.go
@@ -0,0 +1,62 @@
+package infrastructure
+
+import (
+	"os"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	c := NewConfig()
+
+	if c.Port != ":8080" {
+		t.Error(c)
+	}
+
+	if c.Driver != "mysql" {
+		t.Error(c)
+	}
+
+	if c.DataSourceName != "root:secret@tcp(127.0.0.1:3306)/" {
+		t.Error(c)
+	}
+
+	if c.DatabaseName != "mackerel" {
+		t.Error(c)
+	}
+}
+
+func TestGetValue(t *testing.T) {
+	if err := os.Setenv("PORT", ":9090"); err != nil {
+		t.Error(err)
+	}
+
+	if err := os.Setenv("DRIVER", "postgresql"); err != nil {
+		t.Error(err)
+	}
+
+	if err := os.Setenv("DATA_SOURCE_NAME", "user:pswd@tcp(localhost:3307)/"); err != nil {
+		t.Error(err)
+	}
+
+	if err := os.Setenv("DATABASE_NAME", "tmpdb"); err != nil {
+		t.Error(err)
+	}
+
+	c := NewConfig()
+
+	if c.Port != ":9090" {
+		t.Error(c)
+	}
+
+	if c.Driver != "postgresql" {
+		t.Error(c)
+	}
+
+	if c.DataSourceName != "user:pswd@tcp(localhost:3307)/" {
+		t.Error(c)
+	}
+
+	if c.DatabaseName != "tmpdb" {
+		t.Error(c)
+	}
+}
