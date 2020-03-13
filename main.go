01b85cf803746d8c9da093955a2e783734ca5bb6
itsubaki <itsubaki@users.noreply.github.com>
2020-03-13 15:34:05 +0900

    Update shutdown hook

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/itsubaki/mackerel-api/pkg/infrastructure"
 )
@@ -17,19 +20,33 @@ func main() {
 
 	handler := infrastructure.NewSQLHandler(config)
 
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	s := &http.Server{
+		Addr:    config.Port,
+		Handler: infrastructure.Router(handler),
+	}
 
 	go func() {
-		<-c
-		if err := handler.Close(); err != nil {
-			panic(err)
+		log.Println("http server listen and serve")
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
 		}
-
-		os.Exit(0)
 	}()
 
-	if err := infrastructure.Router(handler).Run(config.Port); err != nil {
-		log.Fatalf("router run: %v", err)
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("http server shutdown: %v\n", err)
 	}
+	log.Println("http server closed")
+
+	if err := handler.Close(); err != nil {
+		log.Fatalf("handler closed: %v\n", err)
+	}
+	log.Println("db disconnected")
+
+	log.Println("shutdown finished")
 }
