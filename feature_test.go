4c88cfa845202106d6f0b60c7dcc091a49572100
itsubaki <itsubaki@users.noreply.github.com>
2020-04-30 18:06:01 +0900

    Update .feature and feature_test.go

--- feature_test.go
+++ feature_test.go
@@ -1,30 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/itsubaki/mackerel-api/pkg/infrastructure"
+	"github.com/itsubaki/mackerel-api/pkg/infrastructure/config"
+	"github.com/itsubaki/mackerel-api/pkg/infrastructure/handler"
 	"net/http/httptest"
+	"reflect"
 
 	"github.com/cucumber/godog"
 	"github.com/cucumber/messages-go/v10"
 )
 
 type APIFeature struct {
-	resp *httptest.ResponseRecorder
+	xapikey string
+	resp    *httptest.ResponseRecorder
 }
 
 func (a *APIFeature) reset(m *messages.Pickle) {
 	a.resp = httptest.NewRecorder()
 }
 
+func (a *APIFeature) SetXAPIKEY(xapikey string) error {
+	a.xapikey = xapikey
+	return nil
+}
+
 func (a *APIFeature) Request(method, endpoint string) error {
 	gin.SetMode(gin.ReleaseMode)
 
-	r := gin.New()
-	infrastructure.Status(r)
+	c := config.New()
+	h := handler.New(c)
+	r := infrastructure.Router(h)
 
 	req := httptest.NewRequest(method, endpoint, nil)
+	req.Header.Add("X-Api-key", a.xapikey)
 	r.ServeHTTP(a.resp, req)
 
 	return nil
@@ -38,10 +50,31 @@ func (a *APIFeature) ResponseCodeShouldBe(code int) error {
 	return fmt.Errorf("expected response code to be: %d, but actual is: %d", code, a.resp.Code)
 }
 
+func (a *APIFeature) ResponseShouldMatchJson(body *messages.PickleStepArgument_PickleDocString) error {
+	var expected, actual interface{}
+
+	if err := json.Unmarshal([]byte(body.Content), &expected); err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(a.resp.Body.Bytes(), &actual); err != nil {
+		return err
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		return fmt.Errorf("expected JSON does not match actual, %v vs. %v", expected, actual)
+	}
+
+	return nil
+}
+
 func FeatureContext(s *godog.Suite) {
 	a := &APIFeature{}
 
 	s.BeforeScenario(a.reset)
+	s.Step(`^I fill the XAPIKEY with "([^"]*)"$`, a.SetXAPIKEY)
 	s.Step(`^I send "([^"]*)" request to "([^"]*)"$`, a.Request)
 	s.Step(`^the response code should be (\d+)$`, a.ResponseCodeShouldBe)
+	s.Step(`^the response should match json:$`, a.ResponseShouldMatchJson)
+
 }
---
+++ features/hosts.feature
@@ -0,0 +1,13 @@
+Feature: get hosts
+  In order to know hosts information
+  As an API user
+  I need to be able to request hosts
+
+  Scenario: should get hosts
+    Given I fill the XAPIKEY with "2684d06cfedbee8499f326037bb6fb7e8c22e73b16bb"
+    When I send "GET" request to "/api/v0/hosts"
+    Then the response code should be 200
+    And the response should match json:
+      """
+      {"hosts":[]}
+      """
