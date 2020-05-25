6187ef457763ec3559fd8ac19e1c1080720fcc8b
itsubaki <itsubaki@users.noreply.github.com>
2020-05-25 10:35:45 +0900

    Fix typo

--- pkg/usecase/downtime_interactor.go
+++ pkg/usecase/downtime_interactor.go
@@ -10,7 +10,7 @@ func (s *DowntimeInteractor) List(orgID string) (*domain.Downtimes, error) {
 	return nil, nil
 }
 
-func (s *DowntimeInteractor) Save(orgID string, downitme *domain.Downtime) (*domain.Downtime, error) {
+func (s *DowntimeInteractor) Save(orgID string, downtime *domain.Downtime) (*domain.Downtime, error) {
 	return nil, nil
 }
 
