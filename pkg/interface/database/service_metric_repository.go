241780bbf133865b2c6babaf316b4ef0fded907d
itsubaki <itsubaki@users.noreply.github.com>
2020-06-02 14:32:56 +0900

    Add gorm to service_metric_repository.go

--- pkg/interface/database/service_metric_repository.go
+++ pkg/interface/database/service_metric_repository.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 
 	"github.com/itsubaki/mackerel-server-go/pkg/domain"
+	"github.com/jinzhu/gorm"
 )
 
 type ServiceMetricRepository struct {
-	SQLHandler
+	DB *gorm.DB
+}
+
+type ServiceMetricValue struct {
+	OrgID       string  `gorm:"column:org_id;       type:varchar(16);  not null; primary_key"`
+	ServiceName string  `gorm:"column:service_name; type:varchar(16);  not null; primary_key"`
+	Name        string  `gorm:"column:name;         type:varchar(128); not null; primary_key"`
+	Time        int64   `gorm:"column:time;         type:bigint;       not null; primary_key"`
+	Value       float64 `gorm:"column:value;        type:double;       not null;"`
 }
 
 func NewServiceMetricRepository(handler SQLHandler) *ServiceMetricRepository {
+	db, err := gorm.Open(handler.Dialect(), handler.Raw())
+	if err != nil {
+		panic(err)
+	}
+	db.LogMode(handler.IsDebugging())
+
 	if err := handler.Transact(func(tx Tx) error {
 		if _, err := tx.Exec(
 			`
@@ -33,97 +48,62 @@ func NewServiceMetricRepository(handler SQLHandler) *ServiceMetricRepository {
 	}
 
 	return &ServiceMetricRepository{
-		SQLHandler: handler,
+		DB: db,
 	}
 }
 
-// select * from service_metrics where service_name=${serviceName} and name=${metricName}
 func (repo *ServiceMetricRepository) Exists(orgID, serviceName, metricName string) bool {
-	rows, err := repo.Query("select 1 from service_metric_values where org_id=? and service_name=? and name=? limit 1", orgID, serviceName, metricName)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	if rows.Next() {
-		return true
+	if repo.DB.Where(&ServiceMetricValue{OrgID: orgID, ServiceName: serviceName, Name: metricName}).First(&ServiceMetricValue{}).RecordNotFound() {
+		return false
 	}
 
-	return false
+	return true
 }
 
-// select distinct name from service_metrics where service_name=${serviceName}
 func (repo *ServiceMetricRepository) Names(orgID, serviceName string) (*domain.ServiceMetricValueNames, error) {
+	result := make([]ServiceMetricValue, 0)
+	if err := repo.DB.Table("service_metric_values").Where(&ServiceMetricValue{OrgID: orgID, ServiceName: serviceName}).Select("distinct(name)").Find(&result).Error; err != nil {
+		return nil, fmt.Errorf("select distinct name from service_metric_values: %v", err)
+	}
+
 	names := make([]string, 0)
-	if err := repo.Transact(func(tx Tx) error {
-		rows, err := repo.Query("select distinct name from service_metric_values where org_id=? and service_name=?", orgID, serviceName)
-		if err != nil {
-			return fmt.Errorf("select distinct name from service_metric_values: %v", err)
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			var name string
-			if err := rows.Scan(&name); err != nil {
-				return fmt.Errorf("scan: %v", err)
-			}
-
-			names = append(names, name)
-		}
-
-		return nil
-	}); err != nil {
-		return nil, fmt.Errorf("transaction: %v", err)
+	for _, r := range result {
+		names = append(names, r.Name)
 	}
 
 	return &domain.ServiceMetricValueNames{Names: names}, nil
 }
 
-// select * from service_metrics where service_name=${serviceName} and name=${metricName}  and ${from} < from and to < ${to}
 func (repo *ServiceMetricRepository) Values(orgID, serviceName, metricName string, from, to int64) (*domain.ServiceMetricValues, error) {
-	values := make([]domain.ServiceMetricValue, 0)
-	if err := repo.Transact(func(tx Tx) error {
-		rows, err := tx.Query("select time, value from service_metric_values where org_id=? and service_name=? and name=? and ? < time and time < ?", orgID, serviceName, metricName, from, to)
-		if err != nil {
-			return fmt.Errorf("select time, value from service_metric_values: %v", err)
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			var time int64
-			var value float64
-			if err := rows.Scan(&time, &value); err != nil {
-				return fmt.Errorf("scan: %v", err)
-			}
-
-			values = append(values, domain.ServiceMetricValue{
-				ServiceName: serviceName,
-				Name:        metricName,
-				Time:        time,
-				Value:       value,
-			})
-		}
-
-		return nil
-	}); err != nil {
-		return nil, fmt.Errorf("transaction: %v", err)
+	result := make([]ServiceMetricValue, 0)
+	if err := repo.DB.Where(&ServiceMetricValue{OrgID: orgID, ServiceName: serviceName, Name: metricName}).Where("? < time and time < ?", from, to).Find(&result).Error; err != nil {
+		return nil, fmt.Errorf("select time, value from service_metric_values: %v", err)
 	}
 
-	return &domain.ServiceMetricValues{Metrics: values}, nil
+	out := make([]domain.ServiceMetricValue, 0)
+	for _, r := range result {
+		out = append(out, domain.ServiceMetricValue{
+			OrgID:       orgID,
+			ServiceName: serviceName,
+			Name:        metricName,
+			Time:        r.Time,
+			Value:       r.Value,
+		})
+	}
+
+	return &domain.ServiceMetricValues{Metrics: out}, nil
 }
 
-// insert into service_metrics values(${serviceName}, ${name}, ${time}, ${value})
 func (repo *ServiceMetricRepository) Save(orgID, serviceName string, values []domain.ServiceMetricValue) (*domain.Success, error) {
-	if err := repo.Transact(func(tx Tx) error {
+	if err := repo.DB.Transaction(func(tx *gorm.DB) error {
 		for i := range values {
-			if _, err := tx.Exec(
-				"insert into service_metric_values values(?, ?, ?, ?, ?)",
-				orgID,
-				serviceName,
-				values[i].Name,
-				values[i].Time,
-				values[i].Value,
-			); err != nil {
+			if err := tx.Create(&ServiceMetricValue{
+				OrgID:       orgID,
+				ServiceName: serviceName,
+				Name:        values[i].Name,
+				Time:        values[i].Time,
+				Value:       values[i].Value,
+			}).Error; err != nil {
 				return fmt.Errorf("insert into service_metric_values: %v", err)
 			}
 		}
