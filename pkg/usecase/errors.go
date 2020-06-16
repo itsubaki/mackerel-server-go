ef0acb313105ca67c2b2c1240632b1fb3cb9d4e4
itsubaki <itsubaki@users.noreply.github.com>
2020-06-16 10:44:36 +0900

    Refactor errors

--- pkg/usecase/errors.go
+++ pkg/usecase/errors.go
@@ -10,97 +10,50 @@ func (e *Err) Error() string {
 	return fmt.Sprint(e.err)
 }
 
-type PermissionDenied struct {
-	Err
-}
+type PermissionDenied struct{ Err }
 
-type ServiceNotFound struct {
-	Err
-}
+type ServiceNotFound struct{ Err }
 
-type RoleNotFound struct {
-	Err
-}
+type RoleNotFound struct{ Err }
 
-type RoleMetadataNotFound struct {
-	Err
-}
+type RoleMetadataNotFound struct{ Err }
 
-type HostNotFound struct {
-	Err
-}
+type HostNotFound struct{ Err }
 
-type HostMetricNotFound struct {
-	Err
-}
+type HostMetricNotFound struct{ Err }
 
-type HostMetadataNotFound struct {
-	Err
-}
+type HostMetadataNotFound struct{ Err }
 
-type ServiceMetricNotFound struct {
-	Err
-}
+type ServiceMetricNotFound struct{ Err }
 
-type ServiceMetricLimitExceeded struct {
-	Err
-}
+type ServiceMetricLimitExceeded struct{ Err }
 
-type ServiceMetadataNotFound struct {
-	Err
-}
+type ServiceMetadataNotFound struct{ Err }
 
-type AlertNotFound struct {
-	Err
-}
+type AlertNotFound struct{ Err }
 
-type UserNotFound struct {
-	Err
-}
+type UserNotFound struct{ Err }
 
-type HostIsRetired struct {
-	Err
-}
+type HostIsRetired struct{ Err }
 
-type InvitationNotFound struct {
-	Err
-}
+type InvitationNotFound struct{ Err }
 
-type ChannelNotFound struct {
-	Err
-}
+type ChannelNotFound struct{ Err }
 
-type NotificationGroupNotFound struct {
-	Err
-}
+type NotificationGroupNotFound struct{ Err }
 
-type DashboardNotFound struct {
-	Err
-}
+type DashboardNotFound struct{ Err }
 
-type InvalidServiceName struct {
-	Err
-}
-type InvalidRoleName struct {
-	Err
-}
+type InvalidServiceName struct{ Err }
 
-type InvalidJSONFormat struct {
-	Err
-}
+type InvalidRoleName struct{ Err }
 
-type AlertLimitOver struct {
-	Err
-}
+type InvalidJSONFormat struct{ Err }
 
-type MetadataTooLarge struct {
-	Err
-}
+type AlertLimitOver struct{ Err }
 
-type MetadataLimitExceeded struct {
-	Err
-}
+type MetadataTooLarge struct{ Err }
 
-type ServiceMetricPostLimitExceeded struct {
-	Err
-}
+type MetadataLimitExceeded struct{ Err }
+
+type ServiceMetricPostLimitExceeded struct{ Err }
