ca2e6161eaf49ae4c4585798a3db31c62d3c2a3c
itsubaki <itsubaki@users.noreply.github.com>
2020-06-19 16:36:10 +0900

    Refactor RunFixture

--- pkg/infrastructure/router.go
+++ pkg/infrastructure/router.go
@@ -254,11 +254,13 @@ func Router(handler database.SQLHandler) *gin.Engine {
 }
 
 func RunFixture(handler database.SQLHandler) error {
-	if _, err := database.NewOrgRepository(handler).Save("4b825dc642c", "hatena"); err != nil {
+	orgID, orgName, apikey := "4b825dc642c", "hatena", "2684d06cfedbee8499f326037bb6fb7e8c22e73b16bb"
+
+	if _, err := database.NewOrgRepository(handler).Save(orgID, orgName); err != nil {
 		return fmt.Errorf("org save: %v", err)
 	}
 
-	if _, err := database.NewAPIKeyRepository(handler).Save("4b825dc642c", "default", "2684d06cfedbee8499f326037bb6fb7e8c22e73b16bb", true); err != nil {
+	if _, err := database.NewAPIKeyRepository(handler).Save(orgID, "default", apikey, true); err != nil {
 		return fmt.Errorf("apikey save: %v", err)
 	}
 
