bd8d92c31faeed254a94a260edd8d661f93520ba
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-07-10 21:01:10 +0900

    Refactor router api

--- features/status.feature
+++ features/status.feature
@@ -3,6 +3,10 @@ Feature:
   As an API user
   I need to be able to request status
 
-  Scenario: should get status code
+  Scenario: should get status
     When I send "GET" request to "/status"
+    Then the response code should be 200
+
+  Scenario: should get status at root
+    When I send "GET" request to "/"
     Then the response code should be 200
--- pkg/infrastructure/router_api.go
+++ pkg/infrastructure/router_api.go
@@ -11,72 +11,72 @@ import (
 func Hosts(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	hosts := controller.NewHostController(handler)
 
-	h := v0.Group("/hosts")
-	h.GET("", func(c *gin.Context) { hosts.List(c) })
-	h.POST("", func(c *gin.Context) { hosts.Save(c) })
+	g := v0.Group("/hosts")
+	g.GET("", func(c *gin.Context) { hosts.List(c) })
+	g.POST("", func(c *gin.Context) { hosts.Save(c) })
 
-	h.GET("/:hostId", func(c *gin.Context) { hosts.Host(c) })
-	h.PUT("/:hostId", func(c *gin.Context) { hosts.Update(c) })
-	h.PUT("/:hostId/role-fullnames", func(c *gin.Context) { hosts.RoleFullNames(c) })
-	h.POST("/:hostId/status", func(c *gin.Context) { hosts.Status(c) })
-	h.POST("/:hostId/retire", func(c *gin.Context) { hosts.Retire(c) })
+	g.GET("/:hostId", func(c *gin.Context) { hosts.Host(c) })
+	g.PUT("/:hostId", func(c *gin.Context) { hosts.Update(c) })
+	g.PUT("/:hostId/role-fullnames", func(c *gin.Context) { hosts.RoleFullNames(c) })
+	g.POST("/:hostId/status", func(c *gin.Context) { hosts.Status(c) })
+	g.POST("/:hostId/retire", func(c *gin.Context) { hosts.Retire(c) })
 
-	h.GET("/:hostId/metric-names", func(c *gin.Context) { hosts.MetricNames(c) })
-	h.GET("/:hostId/metrics", func(c *gin.Context) { hosts.MetricValues(c) })
+	g.GET("/:hostId/metric-names", func(c *gin.Context) { hosts.MetricNames(c) })
+	g.GET("/:hostId/metrics", func(c *gin.Context) { hosts.MetricValues(c) })
 	v0.GET("/tsdb/latest", func(c *gin.Context) { hosts.MetricValuesLatest(c) })
 	v0.POST("/tsdb", func(c *gin.Context) { hosts.SaveMetricValues(c) })
 
-	h.GET("/:hostId/metadata", func(c *gin.Context) { hosts.ListMetadata(c) })
-	h.GET("/:hostId/metadata/:namespace", func(c *gin.Context) { hosts.Metadata(c) })
-	h.PUT("/:hostId/metadata/:namespace", func(c *gin.Context) { hosts.SaveMetadata(c) })
-	h.DELETE("/:hostId/metadata/:namespace", func(c *gin.Context) { hosts.DeleteMetadata(c) })
+	g.GET("/:hostId/metadata", func(c *gin.Context) { hosts.ListMetadata(c) })
+	g.GET("/:hostId/metadata/:namespace", func(c *gin.Context) { hosts.Metadata(c) })
+	g.PUT("/:hostId/metadata/:namespace", func(c *gin.Context) { hosts.SaveMetadata(c) })
+	g.DELETE("/:hostId/metadata/:namespace", func(c *gin.Context) { hosts.DeleteMetadata(c) })
 }
 
 func Services(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	services := controller.NewServiceController(handler)
 
-	s := v0.Group("/services")
-	s.GET("", func(c *gin.Context) { services.List(c) })
-	s.POST("", func(c *gin.Context) { services.Save(c) })
-	s.DELETE("/:serviceName", func(c *gin.Context) { services.Delete(c) })
+	g := v0.Group("/services")
+	g.GET("", func(c *gin.Context) { services.List(c) })
+	g.POST("", func(c *gin.Context) { services.Save(c) })
+	g.DELETE("/:serviceName", func(c *gin.Context) { services.Delete(c) })
 
-	s.GET("/:serviceName/roles", func(c *gin.Context) { services.ListRole(c) })
-	s.POST("/:serviceName/roles", func(c *gin.Context) { services.SaveRole(c) })
-	s.DELETE("/:serviceName/roles/:roleName", func(c *gin.Context) { services.DeleteRole(c) })
+	g.GET("/:serviceName/roles", func(c *gin.Context) { services.ListRole(c) })
+	g.POST("/:serviceName/roles", func(c *gin.Context) { services.SaveRole(c) })
+	g.DELETE("/:serviceName/roles/:roleName", func(c *gin.Context) { services.DeleteRole(c) })
 
-	s.GET("/:serviceName/metric-names", func(c *gin.Context) { services.MetricNames(c) })
-	s.GET("/:serviceName/metrics", func(c *gin.Context) { services.MetricValues(c) })
-	s.POST("/:serviceName/tsdb", func(c *gin.Context) { services.SaveMetricValues(c) })
+	g.GET("/:serviceName/metric-names", func(c *gin.Context) { services.MetricNames(c) })
+	g.GET("/:serviceName/metrics", func(c *gin.Context) { services.MetricValues(c) })
+	g.POST("/:serviceName/tsdb", func(c *gin.Context) { services.SaveMetricValues(c) })
 
-	s.GET("/:serviceName/metadata", func(c *gin.Context) { services.ListMetadata(c) })
-	s.GET("/:serviceName/metadata/:namespace", func(c *gin.Context) { services.Metadata(c) })
-	s.PUT("/:serviceName/metadata/:namespace", func(c *gin.Context) { services.SaveMetadata(c) })
-	s.DELETE("/:serviceName/metadata/:namespace", func(c *gin.Context) { services.DeleteMetadata(c) })
+	g.GET("/:serviceName/metadata", func(c *gin.Context) { services.ListMetadata(c) })
+	g.GET("/:serviceName/metadata/:namespace", func(c *gin.Context) { services.Metadata(c) })
+	g.PUT("/:serviceName/metadata/:namespace", func(c *gin.Context) { services.SaveMetadata(c) })
+	g.DELETE("/:serviceName/metadata/:namespace", func(c *gin.Context) { services.DeleteMetadata(c) })
 
-	s.GET("/:serviceName/roles/:roleName/metadata", func(c *gin.Context) { services.ListRoleMetadata(c) })
-	s.GET("/:serviceName/roles/:roleName/metadata/:namespace", func(c *gin.Context) { services.RoleMetadata(c) })
-	s.PUT("/:serviceName/roles/:roleName/metadata/:namespace", func(c *gin.Context) { services.SaveRoleMetadata(c) })
-	s.DELETE("/:serviceName/roles/:roleName/metadata/:namespace", func(c *gin.Context) { services.DeleteRoleMetadata(c) })
+	g.GET("/:serviceName/roles/:roleName/metadata", func(c *gin.Context) { services.ListRoleMetadata(c) })
+	g.GET("/:serviceName/roles/:roleName/metadata/:namespace", func(c *gin.Context) { services.RoleMetadata(c) })
+	g.PUT("/:serviceName/roles/:roleName/metadata/:namespace", func(c *gin.Context) { services.SaveRoleMetadata(c) })
+	g.DELETE("/:serviceName/roles/:roleName/metadata/:namespace", func(c *gin.Context) { services.DeleteRoleMetadata(c) })
 }
 
 func Monitors(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	monitors := controller.NewMonitorController(handler)
 
-	m := v0.Group("/monitors")
-	m.GET("", func(c *gin.Context) { monitors.List(c) })
-	m.POST("", func(c *gin.Context) { monitors.Save(c) })
-	m.GET("/:monitorId", func(c *gin.Context) { monitors.Monitor(c) })
-	m.PUT("/:monitorId", func(c *gin.Context) { monitors.Update(c) })
-	m.DELETE("/:monitorId", func(c *gin.Context) { monitors.Delete(c) })
+	g := v0.Group("/monitors")
+	g.GET("", func(c *gin.Context) { monitors.List(c) })
+	g.POST("", func(c *gin.Context) { monitors.Save(c) })
+	g.GET("/:monitorId", func(c *gin.Context) { monitors.Monitor(c) })
+	g.PUT("/:monitorId", func(c *gin.Context) { monitors.Update(c) })
+	g.DELETE("/:monitorId", func(c *gin.Context) { monitors.Delete(c) })
 }
 
 func Channels(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	channels := controller.NewChannelController(handler)
 
-	c := v0.Group("/channels")
-	c.GET("", func(c *gin.Context) { channels.List(c) })
-	c.POST("", func(c *gin.Context) { channels.Save(c) })
-	c.DELETE("/:channelId", func(c *gin.Context) { channels.Delete(c) })
+	g := v0.Group("/channels")
+	g.GET("", func(c *gin.Context) { channels.List(c) })
+	g.POST("", func(c *gin.Context) { channels.Save(c) })
+	g.DELETE("/:channelId", func(c *gin.Context) { channels.Delete(c) })
 }
 
 func NotificationGroups(v0 *gin.RouterGroup, handler database.SQLHandler) {
@@ -92,8 +92,8 @@ func NotificationGroups(v0 *gin.RouterGroup, handler database.SQLHandler) {
 func Graphs(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	graphs := controller.NewGraphController(handler)
 
-	d := v0.Group("/graph-defs")
-	d.POST("/create", func(c *gin.Context) { graphs.SaveDef(c) })
+	g := v0.Group("/graph-defs")
+	g.POST("/create", func(c *gin.Context) { graphs.SaveDef(c) })
 
 	a := v0.Group("/graph-annotations")
 	a.GET("", func(c *gin.Context) { graphs.List(c) })
@@ -105,64 +105,64 @@ func Graphs(v0 *gin.RouterGroup, handler database.SQLHandler) {
 func CheckReports(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	reports := controller.NewCheckReportController(handler)
 
-	r := v0.Group("/monitoring/checks/report")
-	r.POST("", func(c *gin.Context) { reports.Save(c) })
+	g := v0.Group("/monitoring/checks/report")
+	g.POST("", func(c *gin.Context) { reports.Save(c) })
 }
 
 func Alerts(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	alerts := controller.NewAlertController(handler)
 
-	a := v0.Group("/alerts")
-	a.GET("", func(c *gin.Context) { alerts.List(c) })
-	a.POST("/:alertId/close", func(c *gin.Context) { alerts.Close(c) })
+	g := v0.Group("/alerts")
+	g.GET("", func(c *gin.Context) { alerts.List(c) })
+	g.POST("/:alertId/close", func(c *gin.Context) { alerts.Close(c) })
 }
 
 func Dashboards(v0 *gin.RouterGroup, handler database.SQLHandler) {
-	dashboard := controller.NewDashboardController(handler)
+	dashboards := controller.NewDashboardController(handler)
 
-	d := v0.Group("/dashboards")
-	d.GET("", func(c *gin.Context) { dashboard.List(c) })
-	d.POST("", func(c *gin.Context) { dashboard.Save(c) })
-	d.GET("/:dashboardId", func(c *gin.Context) { dashboard.Dashboard(c) })
-	d.PUT("/:dashboardId", func(c *gin.Context) { dashboard.Update(c) })
-	d.DELETE("/:dashboardId", func(c *gin.Context) { dashboard.Delete(c) })
+	g := v0.Group("/dashboards")
+	g.GET("", func(c *gin.Context) { dashboards.List(c) })
+	g.POST("", func(c *gin.Context) { dashboards.Save(c) })
+	g.GET("/:dashboardId", func(c *gin.Context) { dashboards.Dashboard(c) })
+	g.PUT("/:dashboardId", func(c *gin.Context) { dashboards.Update(c) })
+	g.DELETE("/:dashboardId", func(c *gin.Context) { dashboards.Delete(c) })
 }
 
 func Invitations(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	invitations := controller.NewInvitationController(handler)
 
-	i := v0.Group("/invitations")
-	i.GET("", func(c *gin.Context) { invitations.List(c) })
-	i.POST("", func(c *gin.Context) { invitations.Save(c) })
-	i.POST("/revoke", func(c *gin.Context) { invitations.Revoke(c) })
+	g := v0.Group("/invitations")
+	g.GET("", func(c *gin.Context) { invitations.List(c) })
+	g.POST("", func(c *gin.Context) { invitations.Save(c) })
+	g.POST("/revoke", func(c *gin.Context) { invitations.Revoke(c) })
 }
 
 func Users(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	users := controller.NewUserController(handler)
 
-	u := v0.Group("/users")
-	u.GET("", func(c *gin.Context) { users.List(c) })
-	u.DELETE("/:userId", func(c *gin.Context) { users.Delete(c) })
+	g := v0.Group("/users")
+	g.GET("", func(c *gin.Context) { users.List(c) })
+	g.DELETE("/:userId", func(c *gin.Context) { users.Delete(c) })
 }
 
 func Organizations(v0 *gin.RouterGroup, handler database.SQLHandler) {
-	org := controller.NewOrgController(handler)
+	orgs := controller.NewOrgController(handler)
 
-	o := v0.Group("/org")
-	o.GET("", func(c *gin.Context) { org.Org(c) })
+	g := v0.Group("/org")
+	g.GET("", func(c *gin.Context) { orgs.Org(c) })
 }
 
 func CheckMonitors(v0 *gin.RouterGroup, handler database.SQLHandler) {
 	check := controller.NewCheckMonitorController(handler)
 
-	host := v0.Group("/monitoring/checks/host-metric")
-	host.GET("", func(c *gin.Context) { check.HostMetric(c) })
+	h := v0.Group("/monitoring/checks/host-metric")
+	h.GET("", func(c *gin.Context) { check.HostMetric(c) })
 
-	conn := v0.Group("/monitoring/checks/connectivity")
-	conn.GET("", func(c *gin.Context) { check.Connectivity(c) })
+	c := v0.Group("/monitoring/checks/connectivity")
+	c.GET("", func(c *gin.Context) { check.Connectivity(c) })
 
-	service := v0.Group("/monitoring/checks/service-metric")
-	service.GET("", func(c *gin.Context) { check.ServiceMetric(c) })
+	s := v0.Group("/monitoring/checks/service-metric")
+	s.GET("", func(c *gin.Context) { check.ServiceMetric(c) })
 
 	ext := v0.Group("/monitoring/checks/external")
 	ext.GET("", func(c *gin.Context) { check.External(c) })
@@ -172,21 +172,21 @@ func CheckMonitors(v0 *gin.RouterGroup, handler database.SQLHandler) {
 }
 
 func Downtimes(v0 *gin.RouterGroup, handler database.SQLHandler) {
-	downtime := controller.NewDowntimeController(handler)
+	downtimes := controller.NewDowntimeController(handler)
 
-	d := v0.Group("/downtimes")
-	d.GET("", func(c *gin.Context) { downtime.List(c) })
-	d.POST("", func(c *gin.Context) { downtime.Save(c) })
-	d.GET("/:downtimeId", func(c *gin.Context) { downtime.Downtime(c) })
-	d.PUT("/:downtimeId", func(c *gin.Context) { downtime.Update(c) })
-	d.DELETE("/:downtimeId", func(c *gin.Context) { downtime.Delete(c) })
+	g := v0.Group("/downtimes")
+	g.GET("", func(c *gin.Context) { downtimes.List(c) })
+	g.POST("", func(c *gin.Context) { downtimes.Save(c) })
+	g.GET("/:downtimeId", func(c *gin.Context) { downtimes.Downtime(c) })
+	g.PUT("/:downtimeId", func(c *gin.Context) { downtimes.Update(c) })
+	g.DELETE("/:downtimeId", func(c *gin.Context) { downtimes.Delete(c) })
 }
 
 func UseAPIKey(g *gin.RouterGroup, handler database.SQLHandler) {
-	apikey := controller.NewAPIKeyController(handler)
+	apikeys := controller.NewAPIKeyController(handler)
 
 	g.Use(func(c *gin.Context) {
-		key, err := apikey.APIKey(c)
+		key, err := apikeys.APIKey(c)
 		if err != nil {
 			c.Status(http.StatusForbidden)
 			c.Abort()
