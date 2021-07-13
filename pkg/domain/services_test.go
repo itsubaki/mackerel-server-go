9d4a73b3ee66c65d9e9047bed4d388dc25007a5a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-07-13 10:58:32 +0900

    Add services test

---
+++ pkg/domain/services_test.go
@@ -0,0 +1,35 @@
+package domain_test
+
+import (
+	"testing"
+
+	"github.com/itsubaki/mackerel-server-go/pkg/domain"
+)
+
+func TestServiceMetricValues(t *testing.T) {
+	smv := &domain.ServiceMetricValues{
+		Metrics: make([]domain.ServiceMetricValue, 0),
+	}
+
+	names := []string{"foo", "bar"}
+	for _, n := range names {
+		smv.Metrics = append(smv.Metrics, domain.ServiceMetricValue{
+			Name: n,
+		})
+	}
+
+	mn := smv.MetricNames()
+	for _, n := range mn.Names {
+		found := false
+		for _, nn := range names {
+			if n == nn {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Error()
+		}
+	}
+}
