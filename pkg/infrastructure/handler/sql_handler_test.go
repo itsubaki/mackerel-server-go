17e65c28b89db0479114aa2780cd678778c12446
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-07-16 22:36:04 +0900

    Update sql handler test

--- pkg/infrastructure/handler/sql_handler_test.go
+++ pkg/infrastructure/handler/sql_handler_test.go
@@ -1,1 +1,25 @@
 package handler_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/itsubaki/mackerel-server-go/pkg/infrastructure/handler"
+)
+
+func TestDSN(t *testing.T) {
+	cases := []struct {
+		host     string
+		database string
+	}{
+		{"root:secret@tcp(127.0.0.1:3306)/", "mackerel_test"},
+	}
+
+	for _, c := range cases {
+		dsn := handler.DSN(c.host, c.database)
+		if dsn != fmt.Sprintf("%s%s", c.host, c.database) {
+			t.Errorf(dsn)
+		}
+	}
+
+}
